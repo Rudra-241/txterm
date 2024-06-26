e64f6758c3deef01e820fa8b22efc6d195612c95
rudra <rudrasaxena2004@gmail.com>
2024-06-26 20:55:15 +0530

    Added functionality to add contact

--- server/middlewares/authentication.js
+++ server/middlewares/authentication.js
@@ -2,14 +2,14 @@ import { validateToken } from "../services/authentication.js";
 
 async function restrictToLoggedinUserOnly(req, res, next) {
   const userUid = req.get("X-sessionID");
-  if (!userUid) return res.redirect("/login");
+  if (!userUid) return res.json({message:"Firstly login please"});
   let user;
   try {
     user = validateToken(userUid);
   } catch (err) {
     return res.json({ error: "Invalid session ID" });
   }
-  if (!user) return res.redirect("/login");
+  if (!user) return res.json({message:"Firstly login please"});
 
   req.user = user;
   next();
