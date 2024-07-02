841f93cad9115b2e517c10d49dc9c5d0ccaaf5fc
rudra <rudrasaxena2004@gmail.com>
2024-07-03 01:52:21 +0530

    added welcome message

--- client/txterm.js
+++ client/txterm.js
@@ -1,8 +1,25 @@
 #!/usr/bin/env node
 import { login, register, chat, addNewFriend } from "./index.js";
+import cfonts from "cfonts";
 import { program } from "commander";
 import pkg from "enquirer";
 const { prompt } = pkg;
+
+cfonts.say("TXTERM!", {
+  font: "block",
+  align: "center",
+  colors: ["red", "#f80"],
+  background: "transparent",
+  letterSpacing: 1,
+  lineHeight: 0.5,
+  space: true,
+  maxLength: "0",
+  gradient: ["red", "#f80"],
+  independentGradient: true,
+  transitionGradient: true,
+  rawMode: false,
+  env: "node",
+});
 
 program
   .version("1.0.0")
@@ -39,7 +56,8 @@ program
   })
   .option("-c, --chat <recipient>", "Start chatting", async (options) => {
     chat(options);
-  }).option("-a, --add <username>", "Add a new friend", async (options) => {
+  })
+  .option("-a, --add <username>", "Add a new friend", async (options) => {
     addNewFriend(options);
   });
 
